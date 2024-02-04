b2235c3c80d0ab93082c911cb33c59d1951e2483
Danil <45513393+Pojerman@users.noreply.github.com>
2024-02-04 16:25:43 +0300

    upd

--- functions/server.js
+++ functions/server.js
@@ -14,11 +14,11 @@ app.use(express.static('images'));
 app.use(express.json());
 app.use(cors());
 
-app.use(authRoutes);
-app.use(filmsRoutes);
-app.use(imagesRoutes);
-app.use(commentsRoutes);
-app.use(favoriteRoutes);
+app.use('/.netlify/functions/server/login', authRoutes);
+app.use('/.netlify/functions/server/films', filmsRoutes);
+app.use('/.netlify/functions/server/images', imagesRoutes);
+app.use('/.netlify/functions/server/comments', commentsRoutes);
+app.use('/.netlify/functions/server/favorite', favoriteRoutes);
 
 app.use('/.netlify/functions/server', router);
 module.exports.handler = serverless(app);
